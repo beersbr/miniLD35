614b16e8cdf653ecfcef5c388c0c19836597512f
Brett Beers <beersbr@gmail.com>
2012-06-17 14:50:03 -0600

    Added an enemy config for different enemy types

---
+++ js/enemy.js
@@ -0,0 +1,35 @@
+EnemyConfig = {}
+
+EnemyConfig.enemy_types = ["normal", "speed", "cirle", "double", "spawner", "diagnal", "jumper"];
+
+EnemyConfig.callback = function(obj){
+	switch(obj.type){
+		case "normal":
+			obj.x += obj.ax;
+			break;
+
+		case "speed": 
+			obj.x += obj.ax * 4;
+			break;
+
+		case "circle": 
+			obj.x += obj.ax * 3;
+			break;
+
+		case "double": 
+			obj.x += obj.ax * 3;
+			break;
+
+		case "spawner": 
+			obj.x += obj.ax * 3;
+			break;
+
+		case "diagnal": 
+			obj.x += obj.ax * 3;
+			break;
+
+		case "jumper":
+			obj.x += obj.ax * 3;
+			break;
+	}
+}
